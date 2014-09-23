a97161d97cf14e6b04bbd7d3fd0d1c4e27bc994f
Stephen Lloyd <stephen@Stephens-MacBook-Pro-2.local>
2014-09-23 17:27:24 +0100

    extended boris bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,12 +1,15 @@
 class DockingStation
 	def initialize
-		#sets an instance varialbe called bikes to the value of an empty array
 		@bikes = []
 	end
 
-	# a method which takes a bike as an argument.
+	# attr_writer(:bikes, :broken_bikes)
+
+	# def bikes=(bikes)
+	# 	@bikes = bikes
+	# end
+
 	def dock(bike)
-		# shovels the bike variable into a method called bikes
 		bikes << bike
 	end
 
@@ -16,6 +19,14 @@ class DockingStation
 
 	def available_bikes
 		bikes.reject{|bike|bike.broken?}
+	end
+
+	def release(bike)
+		bikes.delete(bike)
+	end
+
+	def retrieve_working_bikes_from(container)
+		container.bikes.each {|bike| dock(container.release(bike)) unless bike.broken?}
 	end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,6 +4,7 @@ describe DockingStation do
 	let(:station){DockingStation.new}
 	let(:bike) {double :bike, broken?: false}
 	let(:broken_bike){double :bike, broken?: true}
+	let(:van) {double :van, bikes: [bike, broken_bike]}
 
 	it "should accept a bike" do
 		station.dock(bike)
@@ -15,4 +16,27 @@ describe DockingStation do
 		station.dock(broken_bike)
 		expect(station.available_bikes).to eq [bike]
 	end
+
+	it "can release a bike" do
+		station.dock(bike)
+		expect{station.release(bike)}.to change{station.bikes.count}.by -1
+	end
+
+	it "can get all working bikes from a van" do
+		allow(van).to receive(:release).with(bike).and_return(bike)
+		station.retrieve_working_bikes_from(van)
+		expect(station.bikes).to eq [bike]
+	end
+
+	
+
+	it "should release working bikes from a van" do 
+		expect(van).to receive(:release).with(bike).and_return(bike)
+		station.retrieve_working_bikes_from(van)
+	end
+
+	it 'should not release a broken bike to a person' do
+		station.dock(broken_bike)
+	end
+
 end
